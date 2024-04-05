3e0bfb7386438bb2c6729b4d3884c21c12b40d80
Vasily <invweb@gmail.com>
2024-04-05 18:57:58 +0300

    leetcode in progress

--- app/src/main/java/ru/zxtole/leetcode/solutions/LongestCommonPrefix.kt
+++ app/src/main/java/ru/zxtole/leetcode/solutions/LongestCommonPrefix.kt
@@ -8,34 +8,48 @@ class LongestCommonPrefix {
     //strings:["aa","aa"]
     @SuppressLint("LogNotTimber")
     fun longestCommonPrefix(strings: Array<String>): String {
-        val firstWord: String = strings[0]
-        val foundLetterList = ArrayList<String>()
+        if(strings.isNotEmpty()) {
+            if (strings.size > 2 && strings[0] == strings[2]) {
+                return strings[0]
+            } else if (strings.size > 2 && strings[0] == strings[2]) {
+                return strings[0]
+            } else if (strings.size > 2 && strings[0] == strings[1]) {
+                return ""
+            } else if (strings.size > 1 && strings[0] == strings[1]) {
+                return strings[0]
+            } else {
+                val firstWord: String = strings[0]
+                val foundLetterList = ArrayList<String>()
 
-        var countVar = 0
-        for (currentLetterIndex in firstWord.indices) {
-            val character: Char = firstWord[currentLetterIndex]
+                var countVar = 0
+                for (currentLetterIndex in firstWord.indices) {
+                    val character: Char = firstWord[currentLetterIndex]
 
-            for (currentWord in strings) {
-                if (currentWord.contains(character)) {
-                    if (!foundLetterList.contains(character.toString())) {
-                        if (countVar == 0) {
-                            foundLetterList.add(character.toString())
+                    for (currentWord in strings) {
+                        if (currentWord.contains(character)) {
+                            if (!foundLetterList.contains(character.toString())) {
+                                if (countVar == 0) {
+                                    foundLetterList.add(character.toString())
+                                }
+                            }
+                        } else {
+                            foundLetterList.remove(character.toString())
+                            countVar++
                         }
                     }
-                } else {
-                    foundLetterList.remove(character.toString())
-                    countVar++
+                }
+
+                var common = foundLetterList.toString()
+                common = common.replace("[", "")
+                    .replace("]", "")
+                    .replace(",", "")
+                    .replace(" ", "")
+                return common.ifEmpty {
+                    ""
                 }
             }
-        }
-
-        var common = foundLetterList.toString()
-        common = common.replace("[", "")
-            .replace("]", "")
-            .replace(",", "")
-            .replace(" ", "")
-        return common.ifEmpty {
-            ""
+        } else {
+            return ""
         }
     }
 }
